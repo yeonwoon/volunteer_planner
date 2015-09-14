3a35af7186b49bb52cfad232737f3c78dbc6e2c8
Christoph Meißner <post@christophmeissner.de>
2015-09-14 12:10:04 +0200

    Add merge migration after reintegration of develop

---
+++ scheduler/migrations/0019_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('scheduler', '0018_auto_20150912_2134'),
+        ('scheduler', '0012_auto_20150910_2123'),
+    ]
+
+    operations = [
+    ]
